fe7edd2dffc93851c2e17a26eb535a8a1348af8c
Hamza Siddiqui <hamza.siddiqui@encore-pay.com>
2022-11-09 23:25:37 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace Sentence_Word_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program to reverse the whole sentence in same place, if word has starting and end character same it will not reverse it.\n");
+            Console.WriteLine("Enter a sentence... Press enter to submit.");
+            string str = Console.ReadLine();
+            string[] words = str.Split(' ');
+            StringBuilder @string = new StringBuilder();
+            string rev;
+
+            Console.WriteLine("\n+++++++++++++++++++++++++++++++++++++++++++++\n");
+
+            for (int i = 0; i <= words.Length - 1; i++)
+            {
+                rev = words[i];
+                if (rev.ToLower()[0] != rev.ToLower()[rev.Length - 1])
+                {
+                    @string.Clear();
+                    for (int j = rev.Length - 1; j >= 0; j--)
+                    {
+                        words[i] = @string.Append(rev[j]).ToString();
+                    }
+                }
+                else
+                {
+                    words[i] = rev;
+                }
+                Console.Write(words[i] + ' ');
+            }
+            Console.WriteLine("\n\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
